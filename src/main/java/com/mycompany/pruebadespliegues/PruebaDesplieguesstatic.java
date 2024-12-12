c04823d5b8c4788c376cd4a7f3f8f804b52a7752
Usuario <samuelrebollolazaro@gmail.com>
2024-12-12 13:59:33 +0100

    Cambio en el output

---
+++ src/main/java/com/mycompany/pruebadespliegues/PruebaDesplieguesstatic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebadespliegues;
+
+/**
+ *
+ * @author Usuario
+ */
+class PruebaDesplieguesstatic {
+    
+    public static void main(String[]args){
+        System.out.print("Esto es un cambio");
+    }
+}
